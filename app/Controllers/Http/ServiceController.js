f00074a50c602eda84d9bfa6252e70b5ffcb7e6b
Raellopes368 <raxbiencute2010@hotmail.com>
2020-02-20 18:49:13 -0300

    Relacionamento de tabelas usando with

--- app/Controllers/Http/ServiceController.js
+++ app/Controllers/Http/ServiceController.js
@@ -1,43 +1,43 @@
-'use strict'
+"use strict";
 
-const Service = use('App/Models/Service')
-const Database = use('Database')
+const Service = use("App/Models/Service");
+const UserService = use("App/Models/UserService");
+const Database = use("Database");
 
 class ServiceController {
-  async index ({ auth }) {
-    const services = await Database
-      .table('user_services')
-      .where('user_id', auth.user.id)
-      .innerJoin('services', 'user_services.service_id', 'services.id')
-      .select('name', 'slug')
-
-    return services
+  async index({ auth }) {
+    const services = await UserService.query()
+      .with("user")
+      .with("services")
+      .where("user_id", auth.user.id)
+      .fetch();
+    return services;
   }
 
-  async store ({ request }) {
-    const data = request.only(['name'])
+  async store({ request }) {
+    const data = request.only(["name"]);
 
-    const service = await Service.create(data)
+    const service = await Service.create(data);
 
-    return service
+    return service;
   }
 
-  async show ({ response, params, auth }) {
-    const service = await Database
-      .table('user_services')
-      .where('user_id', auth.user.id)
-      .where('service_id', params.id)
-      .innerJoin('services', 'user_services.service_id', 'services.id')
-      .select('name', 'slug')
+  async show({ response, params, auth }) {
+    const service = await UserService.query()
+      .with("user")
+      .with("services")
+      .where("user_id", auth.user.id)
+      .where("service_id", params.id)
+      .fetch();
 
     if (service.length === 0) {
       return response.status(401).send({
-        message: 'Você não possui autorização para acessar este serviço'
-      })
+        message: "Você não possui autorização para acessar este serviço"
+      });
     }
 
-    return service
+    return service;
   }
 }
 
-module.exports = ServiceController
+module.exports = ServiceController;
